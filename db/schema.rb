6011bbf33e0021cbb05421b82da1b607c37ad2ca
kegrimes <kegrimes02@gmail.com>
2025-05-21 15:11:31 -0400

    add article_category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_20_181711) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_21_185952) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
